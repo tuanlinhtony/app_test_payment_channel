764f83df83cce6e4e1c5edfc9a32a48ac0857796
tuanlinhtony <mrpi3990@gmail.com>
2022-10-13 09:44:48 +0700

    Change contribute of connect moongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true  
-})   
-.then(() => console.log("Database connected!"))
-.catch(err => console.log(err))
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log("Database connected!"))
+// .catch(err => console.log(err))
+
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err))
