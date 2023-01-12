df17929a90ac98f1b4a4b1151ff5791457fda0c1
tuanlinhtony <mrpi3990@gmail.com>
2023-01-12 12:24:45 +0700

    Change api endpoint

--- src/public/js/my_js/qr_code_payment.js
+++ src/public/js/my_js/qr_code_payment.js
@@ -12,11 +12,12 @@ const chargeMoneyMobile = document.querySelector('.chargeMoneyMobile')
 chargeMobileSubmit.addEventListener('submit', (e) => {
     e.preventDefault()
         const dataRequest = {
-            merchantCode: inputMerchantCode.value,
-            merchantKey: inputApiToken.value,
+            merchantCode: "869c61b9-480e-4c2b-be46-55522842b951",
+            merchantKey: "2KtvWirwvu3anc2",
             transactionId: inputTransactionID.value,
             moneyRequest: inputAmount.value,
-            customerIp: "123.234.445"
+            customerIp: "123.234.445",
+            type: "payin"
         }
         console.log(inputAmount.value)
         console.log(inputTransactionID.value)
@@ -26,11 +27,12 @@ chargeMobileSubmit.addEventListener('submit', (e) => {
         loadingShowBox.style.display = "block"
 
         const RedirectURL = () => {
-            window.location.href = "https://spayteam.com/payment/" + sign;
+            // window.location.href = "https://spayteam.com/payment/" + sign;
+            // window.open("https://spayteam.com/payment/" + sign);
         }
 
         setTimeout(RedirectURL, 5000)
-        fetch("https://api.spayteam.pro/v1/payin", {
+        fetch("https://api.spayteam.pro/v1/pay", {
             method: 'POST',
             body: JSON.stringify(dataRequest),
             headers: { 'Content-Type': 'application/json' }
@@ -43,6 +45,7 @@ chargeMobileSubmit.addEventListener('submit', (e) => {
                 }else{
                     console.log(data)
                     sign = data.sign.toString()
+                    window.open("https://spayteam.com/payment/" + sign);
                 }
             })
         })
